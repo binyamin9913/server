c6f11b3ba753a844788204ef446f1a1c581fbd1b
binyamin9913 <58781448+binyamin9913@users.noreply.github.com>
2020-01-07 20:39:38 +0200

    v1

--- server.py
+++ server.py
@@ -1,1 +1,18 @@
-jjjj
+from flask import Flask
+from flask import request
+from flask import render_template
+import urllib.parse
+text=""
+app = Flask(__name__)
+@app.route('https://binyamin9913.github.io/server')
+@app.route('https://binyamin9913.github.io/server', methods=['POST'])
+def response():
+    text = request.date
+     return flask.render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+text.encode(encoding='UTF-8',errors='strict')   
+urllib.parse.quote(url_text)
